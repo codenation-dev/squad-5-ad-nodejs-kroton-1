87b29024b7e50f9d00b18243e2d7d43b394bea24
Ronielson Macedo <ronielsonmacedo@gmail.com>
2020-03-03 21:54:35 -0300

    Config do banco e de carregamento dos model.

---
+++ src/models/index.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const path = require('path')
+
+const config = require('../config')
+
+const sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  {
+    ...config.db,
+    dialect: 'mysql'
+  }
+)
+
+const Logs = sequelize.import(
+  path.join(__dirname, 'logs.js')
+)
+
+const Users = sequelize.import(
+  path.join(__dirname, 'users.js')
+)
+
+
+Users.hasMany(Logs)
+Logs.belongsTo(Users)
+
+const db = {}
+
+db[Users.name] = Users
+db[Logs.title] = Logs
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
