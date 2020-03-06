a040be1b968a4c3f90c07737aacdf4ad76b7ff8c
Henrique Marti <henrique@marti.com.br>
2020-03-06 11:45:55 -0300

    Correção na indentação

--- src/controllers/logs.js
+++ src/controllers/logs.js
@@ -25,27 +25,27 @@ Logs.getAll = async (req, res, next) => {
 }
 
 Logs.getById = async (req, res, next) => {
-    const { logId } = req.params
-    try {
-      const result = await logsModel.findOne({
-        where: { 
-          id: logId 
-        },
-        attributes: ['id', 'title', 'detail', 'level', 'events', 'environment', 'source_address', 'archived', 'createdAt', 'updatedAt', 'deletedAt'],
-        include: [{
-          model: usersModel,
-          attributes: ['id', 'name']
-        }]      
-      })
-    
-      if (result === null) {
-        res.status(400).json({ error: `Log is not found`})
-      }
-
-      res.status(200).json(result)
-    } catch(e) {
-      res.status(400).json({ error: e.parent.sqlMessage })
+  const { logId } = req.params
+  try {
+    const result = await logsModel.findOne({
+      where: { 
+        id: logId 
+      },
+      attributes: ['id', 'title', 'detail', 'level', 'events', 'environment', 'source_address', 'archived', 'createdAt', 'updatedAt', 'deletedAt'],
+      include: [{
+        model: usersModel,
+        attributes: ['id', 'name']
+      }]      
+    })
+  
+    if (result === null) {
+      res.status(400).json({ error: `Log is not found`})
     }
+
+    res.status(200).json(result)
+  } catch(e) {
+    res.status(400).json({ error: e.parent.sqlMessage })
+  }
 }
 
 Logs.create = async (req, res, next) => {
@@ -73,29 +73,29 @@ Logs.delete = async (req, res, next) => {
 }
 
 Logs.archive = async (req, res, next) => {
-    const { logId } = req.params
-    const { archived } = req.body
+  const { logId } = req.params
+  const { archived } = req.body
 
-    if (archived !== undefined) {
-      const result = await logsModel.findOne({
-        where: { id: logId }
-      })
-      
-      if (result === null) {
-        res.status(400).json({ error: `Log is not found`})
-      }
+  if (archived !== undefined) {
+    const result = await logsModel.findOne({
+      where: { id: logId }
+    })
     
-      console.log(archived)
-
-      result.archived = archived
-      await result.save()
-      
-      res.status(204).json({})
-    } else {
-        res.status(400).json({
-            error: `The 'archive' field was not sent`
-        })
+    if (result === null) {
+      res.status(400).json({ error: `Log is not found`})
     }
+  
+    console.log(archived)
+
+    result.archived = archived
+    await result.save()
+    
+    res.status(204).json({})
+  } else {
+      res.status(400).json({
+          error: `The 'archive' field was not sent`
+      })
+  }
 }
 
 module.exports = Logs
