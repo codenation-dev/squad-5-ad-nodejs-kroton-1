49dbc05c76125a67dd0ae73b9fd28632b7b6163c
Samuel Batista <samuel.faeng@gmail.com>
2020-03-06 20:48:15 -0300

    CRUD_basico_controller_users_sem_autenticaçao

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,11 @@ const logs = require('./logs')
 router.get('/', (req, res) => {
   res.json({
     users: 'http://localhost:8080/v1/users',
-    logs: 'http://localhost:8080/v1/logs'
+    logs: 'http://localhost:8080/v1/logs',
   })
 })
 
 router.use('/users', users)
 router.use('/logs', logs)
 
-<<<<<<< HEAD
 module.exports = router 
-=======
-module.exports = router
->>>>>>> 29f8e7248bf0afd63c3e04503faa60cf526804a4
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,20 +8,14 @@ router.get('/:userId', controller.getById)
 
 router.post('/', controller.create)
 
-<<<<<<< HEAD
-router.pach('/:userId', controller.findOne)
-=======
-router.patch('/:userId', controller.getById)
->>>>>>> 29f8e7248bf0afd63c3e04503faa60cf526804a4
+//router.post('/login', authentication.findOne)
 
-router.patch('/:userId/changePass', controller.update)
+router.patch('/:userId', controller.update)
 
-router.patch('/:userId/forgottenPass', controller.update)
+//router.patch('/:userId/changePass', controller.update)
+
+//router.patch('/:userId/forgottenPass', controller.update)
 
 router.delete('/:userId', controller.delete)
 
-<<<<<<< HEAD
-module.exports = router 
-=======
 module.exports = router
->>>>>>> 29f8e7248bf0afd63c3e04503faa60cf526804a4
