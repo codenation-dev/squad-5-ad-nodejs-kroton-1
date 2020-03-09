2449e83b668e42706cb95b83e3d14021e475a906
Henrique Marti <henrique@marti.com.br>
2020-03-09 15:47:09 -0300

    Correção na indentação (removi um TAB no inicio das linhas)

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -7,75 +7,75 @@ const jwt = require('jsonwebtoken')
 let Users = {}
 
 Users.getAll = async (req, res, next) => {
-    const data = await model.findAll({
-        attributes: ['id', 'name', 'token', 'createdAt', 'updatedAt']
-      })
-      res.status(200).json({
-        total: data.length,
-        data    
-      })
-  }
+  const data = await model.findAll({
+      attributes: ['id', 'name', 'token', 'createdAt', 'updatedAt']
+    })
+    res.status(200).json({
+      total: data.length,
+      data    
+    })
+}
 
 Users.getById = async (req, res, next) => {
-    const id = req.params.userId
-    return model.findOne({
-        where:{ id },
-        attributes: ['id', 'name', 'token', 'createdAt', 'updatedAt']
-      })
-      .then(result => {
-        if(!result) {
-          return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
-        }
-        return res.status(200).json(result)
-      })
-      .catch(next)
-  }
+  const id = req.params.userId
+  return model.findOne({
+      where:{ id },
+      attributes: ['id', 'name', 'token', 'createdAt', 'updatedAt']
+    })
+    .then(result => {
+      if(!result) {
+        return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
+      }
+      return res.status(200).json(result)
+    })
+    .catch(next)
+}
 
 Users.create = async (req, res, next) => {
-    let {name, email, password} = req.body
-    if(name.length <= 0 || email.length <= 0 || password.length <= 0) {
-        return res.json({error:'Os campos não podem estar em branco'})
-    }
-    const findUser = await model.findOne({
-        where:{email}
-    })
-    if(findUser) {
-        return res.json({error:'E-mail ja cadastrado'})
-    }
-    if(password.length < 8) {
-        return res.json({error:'A senha deve possuir 8 caracteres ou mais'})
-    }
-    password = md5(password)
-    const token = crypto.randomBytes(20).toString('hex')
-    
-    return (model.create({name, email, password, token}))
-      .then(result => res.status(201).json(result))
-      .catch(next)
+  let {name, email, password} = req.body
+  if(name.length <= 0 || email.length <= 0 || password.length <= 0) {
+      return res.json({error:'Os campos não podem estar em branco'})
   }
+  const findUser = await model.findOne({
+      where:{email}
+  })
+  if(findUser) {
+      return res.json({error:'E-mail ja cadastrado'})
+  }
+  if(password.length < 8) {
+      return res.json({error:'A senha deve possuir 8 caracteres ou mais'})
+  }
+  password = md5(password)
+  const token = crypto.randomBytes(20).toString('hex')
+  
+  return (model.create({name, email, password, token}))
+    .then(result => res.status(201).json(result))
+    .catch(next)
+}
 
 Users.update = async (req, res, next) => {
-    const id = req.params.userId
-    let data = req.body
-    return model.update(data, {where: {id}})
-      .then(result => {
-        if(!result[0]) {
-          return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
-        }
-        return res.status(200).json({data})
-      })
-      .catch(next)
-  }
+  const id = req.params.userId
+  let data = req.body
+  return model.update(data, {where: {id}})
+    .then(result => {
+      if(!result[0]) {
+        return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
+      }
+      return res.status(200).json({data})
+    })
+    .catch(next)
+}
 
 Users.delete = async (req, res, next) => {
-    const id = req.params.userId
-    return model.destroy({where:{id}})
-      .then(result => {
-        if(result === 0) {
-          return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
-        }
-        return res.status(200)
-      })
-      .catch(next)
-  }
+  const id = req.params.userId
+  return model.destroy({where:{id}})
+    .then(result => {
+      if(result === 0) {
+        return res.status(404).json({ error: `The record ${id} couldn't be found.` })  
+      }
+      return res.status(200)
+    })
+    .catch(next)
+}
 
 module.exports = Users
