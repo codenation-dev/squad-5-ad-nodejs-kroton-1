6e5dea177954b872bb69852e27053935a694df65
Henrique Marti <henrique@marti.com.br>
2020-03-23 01:51:34 -0300

    Novo Model para controle do reset de senha do usuário e seus vínculos

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,14 @@ const dbConfig = require('../config/index')
 const Users = require('../models/users')
 const Logs = require('../models/logs')
 const Applications = require('../models/applications')
+const passwordReset = require('../models/passwordReset')
 
 const connection = new Sequelize(dbConfig)
 
 Users.init(connection)
 Logs.init(connection)
 Applications.init(connection)
+passwordReset.init(connection)
 
 Users.associate(connection.models)
 Applications.associate(connection.models)
