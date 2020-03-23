daac2903489b7c5e446c0a0daf906680612d4e71
Henrique Marti <henrique@marti.com.br>
2020-03-23 01:52:45 -0300

    Novo módulo para envio de e-mails [conta de e-mail já configurada]

---
+++ src/libs/email.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const EMail = {}
+
+// Estou deixando esses dados aqui para facilitar para todo mundo
+const EMAIL = 'sentinellog@marti.com.br'
+const PASSWORD = 'sentinellog123'
+const SMTP = 'mail.marti.com.br'
+const PORT = 465
+
+EMail.send = async (email, subject, body) => {
+    const transporter = nodemailer.createTransport({
+        host: SMTP,
+        port: PORT,
+        secure: true,
+        auth: {
+            user: EMAIL,
+            pass: PASSWORD,
+        }
+    });
+    
+    const info = await transporter.sendMail({
+        from: `"Sentinel Log" <${EMAIL}>`,
+        to: `${email}`,
+        subject: subject,
+        html: body,
+    });
+
+    return info
+}
+
+module.exports = EMail
