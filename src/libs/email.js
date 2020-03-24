5f238d113259618f484f9d676324d3b89d33c11a
Rogerio <rooh-gerio@hotmail.com>
2020-03-24 14:00:36 -0300

    Alteração do e-mail

--- src/libs/email.js
+++ src/libs/email.js
@@ -2,10 +2,15 @@ const nodemailer = require("nodemailer");
 
 const EMail = {}
 
+// E-mail criado pelo Henrique
+// const EMAIL = 'sentinellog@marti.com.br'
+// const PASSWORD = 'sentinellog123'
+// const SMTP = 'mail.marti.com.br'
+
 // Estou deixando esses dados aqui para facilitar para todo mundo
-const EMAIL = 'sentinellog@marti.com.br'
-const PASSWORD = 'sentinellog123'
-const SMTP = 'mail.marti.com.br'
+const EMAIL = 'sentinellog23@gmail.com'
+const PASSWORD = 'Sentinellog123'
+const SMTP = 'smtp.gmail.com'
 const PORT = 465
 
 EMail.send = async (email, subject, body) => {
