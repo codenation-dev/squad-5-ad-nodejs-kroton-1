0f28012750b11a3633b3e730e283770b21000f12
Samuel Batista <samuel.faeng@gmail.com>
2020-03-24 15:16:13 -0300

    config database local and heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,19 @@
 require('dotenv').config({path: 'variables.env'})
 
+let Database
+
+if (`${process.env.NODE_ENV}` !== 'production' && 'prod'){
+    Database = `sentinel_log_${process.env.NODE_ENV}`
+} else {
+    Database = 'ft48eyekd474dndk'
+}
+
 module.exports = {
     dialect:process.env.DB_DIALECT,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: `sentinel_log_${process.env.NODE_ENV}`,
+    database: Database,
     port:process.env.DB_PORT,
     define:{
         timestamps:true,
