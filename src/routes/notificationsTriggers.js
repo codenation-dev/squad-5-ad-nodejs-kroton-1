e526c0a0878779107c1c3f4da849edfc57584c3a
Henrique Marti <henrique@marti.com.br>
2020-03-25 18:02:53 -0300

    Implementação da Rota e Controller de NotificationTriggers

--- src/routes/notificationsTriggers.js
+++ src/routes/notificationsTriggers.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-/*
+
 const controller = require('../controllers/notificationsTriggers')
 
 router.get('/', controller.getAll)
@@ -12,5 +12,5 @@ router.post('/', controller.create)
 router.patch('/:triggerId', controller.update)
 
 router.delete('/:triggerId', controller.delete)
-*/
+
 module.exports = router
