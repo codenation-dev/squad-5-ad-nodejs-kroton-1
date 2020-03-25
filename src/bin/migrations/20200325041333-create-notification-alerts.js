297c1ee6cad3bf97bdbbe153feb6e1179bc22404
Henrique Marti <henrique@marti.com.br>
2020-03-25 05:22:32 -0300

    Correção do nome do campo para "message"

--- src/bin/migrations/20200325041333-create-notification-alerts.js
+++ src/bin/migrations/20200325041333-create-notification-alerts.js
@@ -25,7 +25,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      messagem: {
+      message: {
         type: Sequelize.TEXT,
         allowNull: false,
       },
