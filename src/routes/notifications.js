cbe8b910c182344bab04da855c2339b94b2f533c
Henrique Marti <henrique@marti.com.br>
2020-03-25 05:25:36 -0300

    Definição das rotas para Notifications, Triggers e Alerts

---
+++ src/routes/notifications.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/notifications')
+const triggers = require('./notificationsTriggers')
+const alerts = require('./notificationsAlerts')
+
+router.get('/', controller.getAll)
+
+router.get('/:notificationId', controller.getById)
+
+router.post('/', controller.create)
+
+router.patch('/:notificationId', controller.update)
+
+router.delete('/:notificationId', controller.delete)
+
+//router.use('/:notificationId/triggers', triggers)
+//router.use('/:notificationId/alerts', alerts)
+
+router.use((err, req, res, next) => {
+    let error = (err.parent || {}).sqlMessage
+    if (err.errors) {
+      error = ''
+      for (const i in err.errors) {
+        if (error !== '') error += ', '
+        error += err.errors[i].message + ''
+      }
+    }
+    res.status(400).json({ error })    
+})
+
+module.exports = router
---
+++ src/routes/notificationsTriggers.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+/*
+const controller = require('../controllers/notificationsTriggers')
+
+router.get('/', controller.getAll)
+
+router.get('/:triggerId', controller.getById)
+
+router.post('/', controller.create)
+
+router.patch('/:triggerId', controller.update)
+
+router.delete('/:triggerId', controller.delete)
+*/
+module.exports = router
