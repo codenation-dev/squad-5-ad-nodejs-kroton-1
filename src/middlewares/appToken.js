862aadf657b6c9b80e60d5cc7e1a285e5e130011
Henrique Marti <henrique@marti.com.br>
2020-03-26 12:31:00 -0300

    Correção da mensagem, estava "user" (antigo) e mudei pra "app"

--- src/middlewares/appToken.js
+++ src/middlewares/appToken.js
@@ -5,7 +5,7 @@ module.exports = {
     const token = req.query.token
 
     if (token === undefined) {
-      return res.status(400).json({ error: `The user token was not sent` })
+      return res.status(400).json({ error: `The app token was not sent` })
     }
     
     const app = await applicationModel.findOne({ where: { token } })
