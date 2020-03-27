25cdc9f6b6c37068a1b268bae60946b7b18ae6fe
Henrique Marti <henrique@marti.com.br>
2020-03-26 22:52:30 -0300

    Remoção do middleware de erro, para usar o geral no index.js

--- src/routes/applications.js
+++ src/routes/applications.js
@@ -12,16 +12,4 @@ router.patch('/:appId', controller.update)
 
 router.delete('/:appId', controller.delete)
 
-router.use((err, req, res, next) => {
-    let error = (err.parent || {}).sqlMessage
-    if (err.errors) {
-      error = ''
-      for (const i in err.errors) {
-        if (error !== '') error += ', '
-        error += err.errors[i].message + ''
-      }
-    }
-    res.status(400).json({ error })    
-})
-
 module.exports = router
